036a67427cee7d32a20d4a07bc9e49017acea201
sara <sara@huhbub.com>
2016-10-29 17:26:16 +0100

    Exercises - only print students whose name begins with a specific letter (Z)

--- directory.rb
+++ directory.rb
@@ -24,7 +24,7 @@ end
 
 def print(students)
   students.each.with_index(1) do |student, index|
-    puts "#{index} #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index} #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with? 'Z'
   end
 end
 
